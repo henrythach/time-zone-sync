1f06ddd162ded0faa1420baa5677f9fc551feeba
Henry Thach <thachhao@gmail.com>
2023-10-07 11:07:01 -0400

    Add hover on card to update time to all

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,9 @@
   import { flip } from 'svelte/animate';
   import moment from 'moment-timezone';
 
+  let timerId: number | null = null;
+  let isHovered = false;
   let currentTime = moment();
-
-  function updateTime() {
-    currentTime = moment();
-  }
 
   function addPerson(name: string, tz: string) {
     people.addPerson(name, tz);
@@ -21,10 +19,64 @@
     people.deleteIndex(index);
   }
 
-  onMount(() => {
+  function handleMouseMove(event: MouseEvent, tz: string) {
+    const rect = event.target?.getBoundingClientRect();
+    const cardWidth = rect.width;
+    const x: number = event.clientX - rect.left;
+
+    // Calculate the percentage of x relative to the card's total width
+    const percentage: number = (x / cardWidth) * 100;
+
+    // Convert that percentage to seconds (0 to 86400, where 86400 is the total seconds in 24 hours)
+    const totalSecondsInDay: number = 24 * 60 * 60;
+    const timeInSeconds: number = Math.floor((percentage / 100) * totalSecondsInDay);
+
+    // Round to the nearest 15-minute increment in seconds
+    const quarterHourInSeconds: number = 15 * 60;
+    const roundedTimeInSeconds: number =
+      Math.round(timeInSeconds / quarterHourInSeconds) * quarterHourInSeconds;
+
+    // Create a Moment object based on the rounded time, using the local time zone
+    let hoveredLocalTime = moment.tz(tz);
+    hoveredLocalTime.startOf('day').add(roundedTimeInSeconds, 'seconds');
+
+    // Update the current time
+    currentTime = hoveredLocalTime.clone();
+  }
+
+  function startInterval() {
+    if (timerId !== null) return; // Already running
+    timerId = setInterval(() => {
+      if (!isHovered) {
+        updateTime();
+      }
+    }, 1000);
+  }
+
+  function stopInterval() {
+    if (timerId !== null) {
+      clearInterval(timerId);
+      timerId = null;
+    }
+  }
+
+  function handleMouseOver() {
+    isHovered = true;
+    stopInterval();
+  }
+
+  function handleMouseOut() {
+    isHovered = false;
     updateTime();
-    const interval = setInterval(updateTime, 1000);
-    return () => clearInterval(interval);
+    startInterval();
+  }
+
+  function updateTime() {
+    currentTime = moment();
+  }
+
+  onMount(() => {
+    startInterval();
   });
 </script>
 
@@ -39,7 +91,17 @@
 
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
     {#each $people as person, index (person)}
-      <div in:scale={{ duration: 200 }} animate:flip={{ duration: 200 }}>
+      <div
+        in:scale={{ duration: 200 }}
+        animate:flip={{ duration: 200 }}
+        on:mouseover={handleMouseOver}
+        on:focus={handleMouseOver}
+        on:mouseout={handleMouseOut}
+        on:blur={handleMouseOut}
+        on:mousemove={(e) => handleMouseMove(e, person.tz)}
+        role="button"
+        tabindex={0}
+      >
         <TimeDisplay
           name={person.name}
           tz={person.tz}
